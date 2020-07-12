77ae693886d1c1a2ffdea15774bb2d7d338b5d95
Zack <warrior.killz@gmail.com>
2020-07-12 14:16:27 +0800

    Completed what_to_eat algo

--- commands/list_food.js
+++ commands/list_food.js
@@ -2,13 +2,49 @@ const mongoose = require('mongoose');
 const Food = require('../models/Food');
 
 exports.run = async (client, message, args) => {
-  // Sort all food by upvotes in descending order
-  const allFood = await Food.find().sort({ upvotes: -1 });
-  const foodChoices = allFood
-      .map(food => `ID ${food.id} - ${food.title} has ${food.upvotes} upvotes, suggested by ${food.author} on ${food.meta.dateCreated.toLocaleDateString("en-US")}`)
-      .join(',\n')
+  try {
+    const loading = await message.channel.send('Loading...');
 
-  message.channel
-    .send(`All Food choices added: \n${foodChoices}`);
+    // Sort all food by upvotes in descending order
+    const allFood = await Food.find().sort({ upvotes: -1 });
+    const foodChoices = allFood
+        .map(food => {
+          return {
+            name: `${food.title} - ID: ${food.id}`,
+            value: `Upvotes: ${food.upvotes}, Author: ${food.author}`
+          }
+        });
+
+    if (!foodChoices) throw new Error("No Food choices have been added yet");
+
+    const embedMessage = {
+      title: 'All Food Choices',
+      color: 13858303,
+      fields: foodChoices,
+      timestamp: new Date(),
+    };
+
+    message.channel
+      .send({
+        embed: embedMessage
+      })
+      .then(() => {
+        // Delete loading message
+        loading.delete();
+      })
+      .catch(console.error);
+  } catch(err) {
+    const embedMessage = {
+      title: 'An error has occurred',
+      color: '#BA1E42',
+      description: `${err.message}`,
+    }
+
+    message.channel
+      .send({
+        embed: embedMessage
+      })
+      .catch(console.error);
+  }
 }
 
--- commands/what_to_eat.js
+++ commands/what_to_eat.js
@@ -3,15 +3,57 @@ const Food = require('../models/Food');
 
 exports.run = async (client, message, args) => {
   try {
+    const loading = await message.channel.send('Deciding for indecisive plebians...');
+
     const allSuggestions = await Food.find();
 
     if (allSuggestions.length === 0) throw new Error('There are no choices in the database');
 
-    const choice = allSuggestions[Math.floor(Math.random() * allSuggestions.length)];
-    
-    message.channel.send(`From my professional BOT opinion, you should have:\n ${choice.title}, upvoted ${choice.upvotes} times`).catch(console.error);
+    // Is there a way to do this functionally loool
+    const weightedSuggestions = [];
+    for (let i = 0; i < allSuggestions.length; i++) {
+      let food = allSuggestions[i];
+      let upvotes = food.upvotes + 1;
+      for (let j = 0; j < upvotes; j++) {
+        weightedSuggestions.push(food);
+      }
+    }
+
+    console.log('weighted suggestions: ', weightedSuggestions.map(food => food.title));
+
+    const choice = weightedSuggestions[Math.floor(Math.random() * allSuggestions.length)];
+
+    const embedMessage = {
+      title: 'BOT has spoken',
+      color: '#42B549',
+      description: `My choice is **${choice.title}** with **${choice.upvotes}** upvotes`,
+      footer: {
+        text: 'If you want this to appear more, upvote this ;)'
+      },
+      timestamp: new Date(),
+    };
+
+    message.channel
+      .send({
+        embed: embedMessage
+      })
+      .then(() => {
+        // Delete loading message
+        loading.delete();
+      })
+      .catch(console.error);
   } catch(err) {
-    message.channel.send(`Error: ${err.message}`);
+    const embedMessage = {
+      title: 'An error has occurred',
+      color: '#BA1E42',
+      description: `${err.message}`,
+    }
+
+    message.channel
+      .send({
+        embed: embedMessage
+      })
+      .catch(console.error);
   }
 }
 
